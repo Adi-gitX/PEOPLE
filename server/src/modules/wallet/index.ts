e254ce2c901c9166ff2a3d27dedd7ca5dc4428f1
adigitX <adityakammati.workspace@gmail.com>
2026-02-26 13:43:47 +0000

    refactor components

---
+++ server/src/modules/wallet/index.ts
@@ -0,0 +1,4 @@
+// Wallet module index
+export { default as walletRoutes } from './wallet.routes.js';
+export * as walletService from './wallet.service.js';
+export * as walletController from './wallet.controller.js';
