e08424d3870f85736b7928c3f2bf7e6033abab03
adigitX <adityakammati.workspace@gmail.com>
2026-02-26 05:14:35 +0000

    update response types

---
+++ server/src/modules/escrow/index.ts
@@ -0,0 +1,4 @@
+// Escrow module index
+export { default as escrowRoutes } from './escrow.routes.js';
+export * as escrowService from './escrow.service.js';
+export * as escrowController from './escrow.controller.js';
