f6ca1c684809739ae182d9ca0a2ac1357bda87f3
adigitX <adityakammati.workspace@gmail.com>
2026-02-27 12:45:38 +0000

    remove deprecated code

--- client/src/hooks/useRealtimeSubscriptions.js
+++ client/src/hooks/useRealtimeSubscriptions.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import { useState, useEffect } from 'react';
 import { collection, query, where, orderBy, limit, onSnapshot } from 'firebase/firestore';
 import { firestore } from '../lib/firebase';
