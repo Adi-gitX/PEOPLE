fcef41f0c62b2d60958c33a9b5d6812904e2925c
adigitX <adityakammati.workspace@gmail.com>
2026-03-02 20:20:57 +0530

    update meta tags

---
+++ server/src/modules/support/index.ts
@@ -0,0 +1,5 @@
+export { supportRoutes, internalSupportRoutes } from './support.routes.js';
+export * as supportController from './support.controller.js';
+export * as supportService from './support.service.js';
+export { supportOutboxWorker } from './support.outbox.worker.js';
+
