cc9d698f4d844266bbceab36bd62ef580bb3c7cb
adigitX <adityakammati.workspace@gmail.com>
2026-03-02 20:20:59 +0530

    update footer links

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,21 @@
 import app from './app.js';
 import { env } from './config/index.js';
 import { runSystemCheck } from './startup/systemCheck.js';
+import { supportOutboxWorker } from './modules/support/index.js';
 
 const PORT = parseInt(env.PORT, 10);
 
 app.listen(PORT, () => {
+    supportOutboxWorker.start();
     void runSystemCheck({ mode: 'runtime', port: PORT });
 });
 
 process.on('SIGTERM', () => {
+    supportOutboxWorker.stop();
     process.exit(0);
 });
 
 process.on('SIGINT', () => {
+    supportOutboxWorker.stop();
     process.exit(0);
 });
